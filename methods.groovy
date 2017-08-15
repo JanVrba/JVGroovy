61c06ce69bc281bbb222633553520de3992dcd53
JanVrba <jan.vrba@gmx.co.uk>
2017-08-15 17:07:08 +0200

    added examples to methods

--- methods.groovy
+++ methods.groovy
@@ -49,3 +49,19 @@ class Example {
       println(ex.getX()); 
    } 
 }
+
+// method with local parameters\
+
+class Example { 
+   static int x = 100; 
+    
+   public static int getX() { 
+      int lx = 200; 
+      println(lx); 
+      return x; 
+   } 
+    
+   static void main(String[] args) { 
+      println(getX());     
+   }  
+}
