7ae4450d87d5cd13268bb379377a4483f03fa3b3
JanVrba <jan.vrba@gmx.co.uk>
2017-08-15 16:47:18 +0200

    added examples to methods

--- methods.groovy
+++ methods.groovy
@@ -29,3 +29,23 @@ def sum(int a,int b){
 }
 d = sum(6,5);
 println(d);
+
+// method inside class
+
+class Example { 
+   int x; 
+    
+   public int getX() { 
+      return x; 
+   } 
+    
+   public void setX(int pX) { 
+      x = pX; 
+   } 
+    
+   static void main(String[] args) { 
+      Example ex = new Example(); 
+      ex.setX(100); 
+      println(ex.getX()); 
+   } 
+}
