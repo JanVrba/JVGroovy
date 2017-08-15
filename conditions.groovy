bea733b1bffcc44b46c96eee38ba7d8e2297d056
JanVrba <jan.vrba@gmx.co.uk>
2017-08-15 16:12:47 +0200

    added examples to conditions

---
+++ conditions.groovy
@@ -0,0 +1,40 @@
+// if statement
+
+i = 4
+if(i < 5) {
+    println("i is less than 5")    
+}
+
+// if else statement
+
+i = 6
+if(i < 5) {
+    println("i is less than 5")
+    } else {
+    println("i is more than 5")  
+}
+
+// else if statement
+
+i = 11
+if(i < 5) {
+    println("i is less than 5")
+    } else if (i < 10) {
+    println("i is less than 10")
+    } else {
+    println("i is more than 10")      
+}
+
+// switch
+
+i = 4
+switch(i) {
+   case 5:    
+     println("i je 5")
+     break;
+   case 4:    
+     println("i je 4")
+     break;
+    default:
+     println("i je neco jineho")
+}
