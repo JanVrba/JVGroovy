1c3b4342dbdb6929ddd2eaf690397039f790c6ee
JanVrba <jan.vrba@gmx.co.uk>
2017-08-16 16:46:34 +0200

    added strings

---
+++ strings.groovy
@@ -0,0 +1,32 @@
+// string indexing
+
+String sample = "Hello world"; 
+for (i=0;i < sample.length();i++) {
+    println(sample[i]);
+}
+
+// string repetition
+
+String sample = "Hello world"; 
+for (i=0;i < sample.length();i++) {
+    println(sample[i]*3);   
+}
+
+// concat
+
+String a = "Hello "
+b = a.concat("World ")
+println(b*3)
+
+// indexOf
+
+String a = "Hello "
+println(a.indexOf('lo')
+
+// toUpper,toLower
+
+String a = "Hello "
+println(a.toUpperCase())
+println(a.toLowerCase())
+
+// string methods : https://www.tutorialspoint.com/groovy/groovy_strings.htm
