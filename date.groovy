9dffe29970cb8f6f71f770fd22f345cd6c1f56c4
JanVrba <jan.vrba@gmx.co.uk>
2017-08-17 09:31:42 +0200

    Added date

--- date.groovy
+++ date.groovy
@@ -3,3 +3,12 @@
 Date datum = new Date()
 println(datum)
 println(datum.toString())
+
+// get date milisec from 1.1.1970
+
+i=0
+while (i<100) {
+    Date datum = new Date()
+    println("${i} : " + datum.getTime() + " milisec")
+    i++
+}
