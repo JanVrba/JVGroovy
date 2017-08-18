c8469c5b3491139e0024da4a648d462d46779b86
JanVrba <jan.vrba@gmx.co.uk>
2017-08-18 13:23:41 +0200

    added closures

--- closures.groovy
+++ closures.groovy
@@ -1,3 +1,22 @@
 // source : https://www.tutorialspoint.com/groovy/groovy_closures.htm
+
 // closure
 
+def clos = {println "Hello World"};
+clos.call();
+
+// closure formal parameters
+
+def clos = {param->println "Hello ${param}"};
+clos.call("World");
+
+// using it keyword
+
+def clos = {println "Hello ${it}"};
+clos.call("World");
+
+// using variables
+
+def str1 = "Hello";
+def clos = {param -> println "${str1} ${param}"}
+clos.call("World");
