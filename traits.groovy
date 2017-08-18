9f396f360c83748b3d688d04bd4bc0d138c55b60
JanVrba <jan.vrba@gmx.co.uk>
2017-08-18 11:51:21 +0200

    added traits

--- traits.groovy
+++ traits.groovy
@@ -53,3 +53,29 @@ class Student implements Marks {
    int StudentID
    int Marks1;  
 } 
+
+// extend traits
+
+class Example {
+   static void main(String[] args) {
+      Student st = new Student();
+      st.StudentID = 1;
+      println(st.DisplayMarks());
+   } 
+} 
+
+trait Marks {
+   void DisplayMarks() {
+      println("Marks1");
+   } 
+} 
+
+trait Total extends Marks {
+   void DisplayMarks() {
+      println("Total");
+   } 
+}  
+
+class Student implements Total {
+   int StudentID 
+}
