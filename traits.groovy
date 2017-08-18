54df73d8e4d0da02dcca00829a58c2295b50a333
JanVrba <jan.vrba@gmx.co.uk>
2017-08-18 11:23:30 +0200

    added generic and traits

---
+++ traits.groovy
@@ -0,0 +1,55 @@
+
+// source : https://www.tutorialspoint.com/groovy/groovy_traits.htm
+// implement trait 
+
+class Example {
+   static void main(String[] args) {
+      Student st = new Student();
+      st.StudentID = 1;
+      st.Marks1 = 10; 
+      println(st.DisplayMarks());
+   } 
+} 
+
+trait Marks { 
+   void DisplayMarks() {
+      println("Display Marks");
+   } 
+} 
+
+class Student implements Marks { 
+   int StudentID
+   int Marks1;
+}
+
+// trait implements interfaces
+
+class Example {
+   static void main(String[] args) {
+      Student st = new Student();
+      st.StudentID = 1;
+      st.Marks1 = 10;
+		
+      println(st.DisplayMarks());
+      println(st.DisplayTotal());
+   } 
+} 
+
+interface Total {
+   void DisplayTotal() 
+} 
+
+trait Marks implements Total {
+   void DisplayMarks() {
+      println("Display Marks");
+   }
+	
+   void DisplayTotal() {
+      println("Display Total"); 
+   } 
+} 
+
+class Student implements Marks { 
+   int StudentID
+   int Marks1;  
+} 
